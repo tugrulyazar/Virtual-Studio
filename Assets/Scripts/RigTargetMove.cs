59e4c97a591dcc5798eb33f20ca7d34e8a3c065f
Can <76254714+CanUnesi@users.noreply.github.com>
2021-07-27 23:14:15 +0300

    Added finger pointing and zoom, worked on animations

--- Assets/Scripts/RigTargetMove.cs
+++ Assets/Scripts/RigTargetMove.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Animations.Rigging;
+using Cinemachine;
 
 public class RigTargetMove : MonoBehaviour
 {
     [SerializeField]
-    [Range(0.0f, 10.0f)]
+    [Range(0f, 10f)]
     private float distance;
+    [SerializeField]
+    [Range(30f, 180f)]
+    private float frontAngle = 120;
 
     [SerializeField]
     private GameObject head;
@@ -16,18 +20,24 @@ public class RigTargetMove : MonoBehaviour
     private GameObject playerPos;
 
     [SerializeField]
-    private GameObject[] AimConstraints;
+    private Rig headRig;
+    [SerializeField]
+    private Rig handRig;
 
-    private List<MultiAimConstraint> MultiAimConstraints = new List<MultiAimConstraint>();
-    private List<float> originalWeights = new List<float>();
-    private Transform headTransform;
     private Transform mainCamera;
 
     private float transitionRate = 2.0f;
-    public bool lookingBehind = false;
+    public bool notLooking = false;
 
     private float dot;
     private Vector3 heading;
+
+    private CinemachineBrain cinemachineBrain;
+    CinemachineVirtualCamera activeCam;
+    private float cameraFov;
+    private float zoomFov = 20;
+    private float originalCameraFov;
+
 
     private void Awake()
     {
@@ -35,42 +45,80 @@ public class RigTargetMove : MonoBehaviour
         mainCamera = Camera.main.transform;
     }
 
-    
     private void Start()
     {
-        // Get original weights
-        foreach (GameObject aim in AimConstraints)
-        {
-            MultiAimConstraint mac = aim.GetComponent<MultiAimConstraint>();
-            MultiAimConstraints.Add(mac);
-            originalWeights.Add(mac.weight);  
-        }
+        cinemachineBrain = Camera.main.GetComponent<CinemachineBrain>();
+        StartCoroutine(getActiveCamera());
     }
 
     void Update()
-    {    
+    {
         // Move target according to camera
         transform.position = head.transform.position + mainCamera.forward * distance;
 
-        // Check if target is behind the player
+        // Get the angle between the camera and player heading
         heading = transform.position - playerPos.transform.position;
         float angle = Vector3.Angle(heading, playerPos.transform.forward);
 
-        // If the target is behind, decrease constraint weight over time
-        if (angle > 100)
+        // If the target is behind, don't look
+        notLooking = (angle > frontAngle) ? true : false;
+        Debug.Log("checking angle");
+
+
+        // Decrease head constraint weights over time, disable unnecessary infinite lerping
+        if (notLooking && headRig.weight != 0)
         {
-            foreach (MultiAimConstraint mac in MultiAimConstraints)
+            headRig.weight = (headRig.weight < 0.01) ? 0 : Mathf.Lerp(headRig.weight, 0, Time.deltaTime * transitionRate);
+        }
+        else if (!notLooking && headRig.weight != 1)
+        {
+            headRig.weight = (headRig.weight > 0.99) ? 1 : Mathf.Lerp(headRig.weight, 1, Time.deltaTime * transitionRate);
+        }
+
+
+        // TODO: Carry this over to camera switch script
+        if (Input.GetKeyDown(KeyCode.F)) StartCoroutine(getActiveCamera());
+
+        // TODO: Switch hands based on player side
+        if (Input.GetKey(KeyCode.Mouse0) && !notLooking)
+        {
+            // Increase hand constraint weights
+            if (handRig.weight != 1)
             {
-                mac.weight = Mathf.Lerp(mac.weight, 0, Time.deltaTime * transitionRate);
+                handRig.weight = (handRig.weight > 0.99) ? 1 : Mathf.Lerp(handRig.weight, 1, Time.deltaTime * transitionRate * 3);
+            }
+
+            // Lerp camera fov to zoom
+            if (cameraFov != zoomFov)
+            {
+                cameraFov = (cameraFov < zoomFov + 0.1f) ? zoomFov : Mathf.Lerp(cameraFov, zoomFov, Time.deltaTime * transitionRate * 2);
+                activeCam.m_Lens.FieldOfView = cameraFov;
             }
         }
-        else // look at the target
+        else if (handRig.weight != 0 && cameraFov != originalCameraFov)
         {
-            for (int i = 0; i < MultiAimConstraints.Count; i++)
+            // Decrease hand constraint weights
+            if (handRig.weight != 0)
             {
-                MultiAimConstraints[i].weight = Mathf.Lerp(MultiAimConstraints[i].weight, originalWeights[i], Time.deltaTime * transitionRate);
+                handRig.weight = (handRig.weight < 0.01) ? 0 : Mathf.Lerp(handRig.weight, 0, Time.deltaTime * transitionRate * 4);
             }
+
+            // Lerp camera fov to original
+            if (cameraFov != originalCameraFov)
+            {
+                cameraFov = (cameraFov > originalCameraFov - 0.1f) ? originalCameraFov : Mathf.Lerp(cameraFov, originalCameraFov, Time.deltaTime * transitionRate * 2);
+                activeCam.m_Lens.FieldOfView = cameraFov;
+            }
+
         }
-        
+    }
+
+    IEnumerator getActiveCamera()
+    {
+        yield return null;
+        activeCam = cinemachineBrain.ActiveVirtualCamera as CinemachineVirtualCamera;
+        originalCameraFov = activeCam.m_Lens.FieldOfView;
+        // Temp camera fov
+        cameraFov = originalCameraFov;
     }
 }
